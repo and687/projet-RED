9a1c0b72389fe1c1b85341322a2d150719e6ee94
andrea amato <andrea.amato@ynov.com>
2026-09-18 13:19:55 +0100

    Ajout du menu principal et choix de la classe

---
+++ src/menus/main_menu.go
@@ -0,0 +1,35 @@
+package main_menu
+
+import (
+    "fmt"
+    character "projet-RED/src/models"
+)
+
+func main() {
+	fmt.Println("bienvenue dans le jeu !")
+	fmt.Println("Choisissez la classe :")
+    fmt.Println("1. Chercheur - PV : 100/200")
+    fmt.Println("2. Pilote -PV : 80/160")
+    fmt.Println("3. Mercenaire -PV : 150/300")
+
+	var choix int
+	fmt.Scan(&choix)
+
+	switch choix {
+case 1:
+    nom := character.AskName()
+    personnage := character.InitCharacter(nom, "chercheur", 100, 1, 200)
+    character.DisplayInfo(personnage)
+case 2:
+    nom := character.AskName()
+    personnage := character.InitCharacter(nom, "pilote", 80, 1, 160)
+    character.DisplayInfo(personnage)
+case 3:
+    nom := character.AskName()
+    personnage := character.InitCharacter(nom, "mercenaire", 150, 1, 300)
+    character.DisplayInfo(personnage)
+default:
+    fmt.Println("Choix invalide")
+}
+}
+
