25cd6f38f99a4370bcb587ee121a81b42506b5af
andrea amato <andrea.amato@ynov.com>
2026-09-18 13:07:36 +0100

    Add InitCharacter function

--- src/models/character.go
+++ src/models/character.go
@@ -9,3 +9,14 @@ type character struct {
 	Level     int
 	HealthMax int
 }
+
+func InitCharacter(name string, classe string, health int, level int, healthmax int) character {
+	return character{
+		Name:      name,
+		Classe:    classe,
+		Health:    health,
+		Level:     level,
+		HealthMax: healthmax,
+	
+	}
+}
