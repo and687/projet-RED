46f5f23ae29d802c640aab51211cfb8c2e126654
andrea amato <andrea.amato@ynov.com>
2026-09-21 10:36:55 +0100

    feat la fonction initgoblin

--- src/data/enemies.go
+++ src/data/enemies.go
@@ -1,3 +1,23 @@
 package data
 
-// TODO: implémenter enemies.go
+type Monster struct {
+	Name string
+	Degats int 
+    Health int
+	HealthMax int 
+	Recompense int 
+}
+
+func InitGoblin(name string, degats int, health int, recompense int, healthmax int) Monster {
+	return Monster{
+		Name:    name,
+		Health:    health,
+		Degats:     degats,
+		HealthMax: healthmax,
+		Recompense: recompense,
+	}
+}
+
+
+	
+
